db63d5563dd95e2d964c460ebf24e15d3d68c1e0
Max <maxduma16@gmail.com>
2023-06-08 16:44:16 +0300

    refactoring controllers

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import {registerValidation} from './validation/auth.js';
-import { validationResult } from 'express-validator';
-import UserModel from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
-
+import * as UserController from './controllers/UserController.js';
 
 mongoose.connect('mongodb+srv://maxduma16:aYYy8AIjTqqYpWAP@cluster1.kehnoa2.mongodb.net/blog?retryWrites=true&w=majority')
 .then(() => {
@@ -20,76 +16,9 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/auth/login', async (req, res) => {
-  try {
-    const user = await UserModel.findOne({email: req.body.email})
-    if (!user) {
-      return res.status(404).json({
-        message: 'User not found'
-      })
-    }
-    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: 'Incorrect credentials'
-      })
-    }
-
-    const token = jwt.sign({_id: user._id}, 'secret123', {expiresIn: '30d'});
-    const {passwordHash, ...userData} = user._doc;
-    res.json({...userData, token});
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: 'Failed login user'
-    });
-  }
-})
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-  
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-  
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-    const user = await doc.save();
-
-    const token = jwt.sign({_id: user._id}, 'secret123', {expiresIn: '30d'});
-    const {passwordHash, ...userData} = user._doc;
-    res.json({...userData, token});
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: 'Failed to register user'
-    });
-  }
-})
-
-app.get('/auth/me', checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
-
-    if (!user) {
-      return res.status(404).json({message: 'User not found'})
-    }
-
-    const {passwordHash, ...userData} = user._doc;
-    res.json({...userData});
-  } catch (error) {
-    return res.status(500).json({message: 'no access'});
-  }
-})
+app.post('/auth/login', UserController.login)
+app.post('/auth/register', registerValidation, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.listen(4444, (err) => {
   if (err) {
