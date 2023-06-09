5bf56461c178f571aec1546e4c3ee168a2eff479
Max <maxduma16@gmail.com>
2023-06-09 14:29:48 +0300

    refactorinf and added validation

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import { registerValidation, loginValidation, postCreateValidation } from './val
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
 import * as PostController from './controllers/PostController.js';
+import multer from 'multer';
+import handleValidationErrors from './utils/handleValidationErrors.js';
+
 
 mongoose.connect('mongodb+srv://maxduma16:aYYy8AIjTqqYpWAP@cluster1.kehnoa2.mongodb.net/blog?retryWrites=true&w=majority')
 .then(() => {
@@ -15,22 +18,40 @@ mongoose.connect('mongodb+srv://maxduma16:aYYy8AIjTqqYpWAP@cluster1.kehnoa2.mong
 
 const app = express();
 
-app.use(express.json())
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
+});
 
-app.post('/auth/register', registerValidation, UserController.register);
-app.post('/auth/login', UserController.login);
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 app.delete('/posts/:id', checkAuth, PostController.remove);
 
 app.listen(4444, (err) => {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
-  console.log('Server OK')
+  console.log('Server OK');
 })
 
